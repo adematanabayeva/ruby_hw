e9a28f7589c4306d4ad39c8187b277809eca6a9a
adematanabayeva <tanabaeva.adema@mail.ru>
2019-07-14 20:47:16 +0600

    Update L4_task5.rb

--- L4_task5.rb
+++ L4_task5.rb
@@ -13,9 +13,14 @@ days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 if y % 400 == 0 || ( y % 4 ==0 && y % 100 != 0)
 	days_in_month[1] = 29 
 end
-sum = 0
-for i in 0..m-2
-	  sum += days_in_month[i]
+# sum = 0
+# for i in 0..m-2
+# 	  sum += days_in_month[i]
+# end
+# sum += day
+if m == 1 
+	sum = day
+else 
+	sum = days_in_month[0..(m-2)].inject(:+) + day
 end
-sum += day
 puts sum
