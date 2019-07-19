9ee1cdc7611ec2989100722d634099057be6d71c
adematanabayeva <tanabaeva.adema@mail.ru>
2019-07-19 13:38:44 +0600

    Update L5_task2.rb

--- L5_task2.rb
+++ L5_task2.rb
@@ -1,13 +1,12 @@
 # Дан массив строк. Строки представляют из себя какое-то слово. 
 # Написать метод, который принимает этот массив, и 
 # вычисляет сумму символов в самом коротком и самом длинном слове массива и выводит на экран.
-def LongShort(strings)
-	words = strings
-	l = []
-	words.each { |i| l.push(i.length)}
-	maxx = l.max
-	minn = l.min
+def long_short(strings)
+	word_sizes = []
+	strings.each { |word| word_sizes.push(word.length)}
+	maxx = word_sizes.max
+	minn = word_sizes.min
 	puts maxx + minn
 end
 a = ['hey', 'cherry', 'banana', 'school', 'kids']
-LongShort(a)
+long_short(a)
