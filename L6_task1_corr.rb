a2993fdd1aeb4168b1025cfa2c5bf1683e1501a0
adematanabayeva <tanabaeva.adema@mail.ru>
2019-08-05 16:25:49 +0600

    Add files via upload

---
+++ L6_task1_corr.rb
@@ -0,0 +1,19 @@
+require 'time'
+
+def ask(string)
+	case string
+	when 'time'
+		puts Time.now.strftime('%H:%M')
+	when 'date'
+		puts Time.now.strftime('%d %B' + ', ' + '%Y')
+	when 'day'
+		puts Time.now.strftime('%A')
+	when 'remaining days'
+		puts 365 - (Time.now.strftime('%j').to_i)
+	when 'remaining weeks'
+		53 - (Time.now.strftime('%U').to_i)
+	end
+end
+puts 'Choose and type: time/date/day/remaining days/remaining weeks'
+string = gets.chomp
+ask(string)
